353745ea60b86814dab5dd82a68596ca12568bad
Sandip Mangukiya <sandip.mangukiya@vnc.biz>
2014-11-05 16:52:42 +0530

    FIX: ODOO 8 : 14114_reform_behavior_application_unproxied_V8

--- addons/vnc_zimbra_connector/task_calendar_sync_server.py
+++ addons/vnc_zimbra_connector/task_calendar_sync_server.py
@@ -12,6 +12,114 @@ import werkzeug.contrib.fixers
 import openerp
 import pytz
 import urllib2
+import threading
+
+def wsgi_xmlrpc(environ, start_response):
+    """ Two routes are available for XML-RPC
+
+    /xmlrpc/<service> route returns faultCode as strings. This is a historic
+    violation of the protocol kept for compatibility.
+
+    /xmlrpc/2/<service> is a new route that returns faultCode as int and is
+    therefore fully compliant.
+    """
+    if environ.get('PATH_INFO') in ['/task', '/calendar']:
+        if not environ.get('HTTP_AUTHORIZATION'):
+            start_response("401 Authorization required",
+                           [('WWW-Authenticate', 'Basic realm="OpenERP"'),
+                            ('cache-control', 'no-cache'),
+                            ('Pragma', 'no-cache'),
+                            ('Expires', 0),
+                            ('Content-Type', 'text/html'),
+                            ('Content-Length', 4),  # len(self.auth_required_msg)
+                            ])
+            return environ
+        auth = environ.get('HTTP_AUTHORIZATION')
+        if auth:
+            scheme, data = auth.split(None, 1)
+            assert scheme.lower() == 'basic'
+            username, password = data.decode('base64').split(':', 1)
+            username = urllib2.unquote(username)
+            password = urllib2.unquote(password)
+            query_str = environ.get('QUERY_STRING')
+            para_final = query_str.split('=')
+            cal_data = ''
+            try:
+                if environ.get('PATH_INFO') == '/task':
+                    cal_data = make_service_call(environ.get('SERVER_NAME'), \
+                                environ.get('SERVER_PORT'), username, \
+                                password, para_final[1], 'task')
+                elif environ.get('PATH_INFO') == '/calendar':
+                    cal_data = make_service_call(environ.get('SERVER_NAME'), \
+                                environ.get('SERVER_PORT'), username, \
+                                password, para_final[1], 'calendar')
+                else:
+                    body = 'Invalid URL'
+                    headers = [
+                               ('content-type', 'text/plain'),
+                               ('content-length', str(len(body))),
+                               ('WWW-Authenticate', 'Basic realm="OpenERP"')
+                               ]
+                    start_response('400 BAD REQUEST', headers)
+                    return [body]
+            except:
+                body = 'Please authenticate'
+                headers = [
+                    ('content-type', 'text/plain'),
+                    ('content-length', str(len(body))),
+                    ('WWW-Authenticate', 'Basic realm="OpenERP"')]
+                start_response('401 Unauthorized', headers)
+                return [body]
+            environ['REMOTE_USER'] = username
+            del environ['HTTP_AUTHORIZATION']
+        start_response("200 OK", [('cache-control', 'no-cache'), \
+                        ('Pragma', 'no-cache'), ('Expires', 0), \
+                        ('Content-Type', 'text/calendar'), \
+                        ('Content-length', len(cal_data)), \
+                        ('Content-Disposition', 'attachment; filename='+\
+                         'task_calender'+'.ics')])
+        return cal_data
+    if environ['REQUEST_METHOD'] == 'POST' and environ['PATH_INFO'].startswith('/xmlrpc/'):
+        length = int(environ['CONTENT_LENGTH'])
+        data = environ['wsgi.input'].read(length)
+
+        # Distinguish betweed the 2 faultCode modes
+        string_faultcode = True
+        if environ['PATH_INFO'].startswith('/xmlrpc/2/'):
+            service = environ['PATH_INFO'][len('/xmlrpc/2/'):]
+            string_faultcode = False
+        else:
+            service = environ['PATH_INFO'][len('/xmlrpc/'):]
+
+        params, method = xmlrpclib.loads(data)
+        return wsgi_server.xmlrpc_return(start_response, service, method, params, string_faultcode)
+
+def application_unproxied(environ, start_response):
+    """ WSGI entry point."""
+    # cleanup db/uid trackers - they're set at HTTP dispatch in
+    # web.session.OpenERPSession.send() and at RPC dispatch in
+    # openerp.service.web_services.objects_proxy.dispatch().
+    # /!\ The cleanup cannot be done at the end of this `application`
+    # method because werkzeug still produces relevant logging afterwards 
+    if hasattr(threading.current_thread(), 'uid'):
+        del threading.current_thread().uid
+    if hasattr(threading.current_thread(), 'dbname'):
+        del threading.current_thread().dbname
+
+    with openerp.api.Environment.manage():
+        # Try all handlers until one returns some result (i.e. not None).
+        wsgi_handlers = [wsgi_server.wsgi_xmlrpc]
+        wsgi_handlers += wsgi_server.module_handlers
+        for handler in wsgi_handlers:
+            result = handler(environ, start_response)
+            if result is None:
+                continue
+            return result
+
+    # We never returned from the loop.
+    response = 'No handler found.\n'
+    start_response('404 Not Found', [('Content-Type', 'text/plain'), ('Content-Length', str(len(response)))])
+    return [response]
 
 def application(environ, start_response):
     if environ.get('PATH_INFO') in ['/task', '/calendar']:
@@ -188,5 +296,7 @@ def make_service_call(host, port, username, pwd, dbname, option):
             cal.add_component(event)
         return cal.to_ical()
 
+wsgi_server.application_unproxied = application_unproxied
+wsgi_server.wsgi_xmlrpc = wsgi_xmlrpc
 wsgi_server.application = application
 # vim:expandtab:smartindent:tabstop=4:softtabstop=4:shiftwidth=4:

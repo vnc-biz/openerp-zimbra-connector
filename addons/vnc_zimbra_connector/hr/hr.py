0aed2842d63b300cc9486301b486a295a40b25b9
Ekta Dakwala <ekta.dakwala@vnc.biz>
2016-04-21 19:07:20 +0530

    [IMP] hr leave sync : added code in res.company

--- addons/vnc_zimbra_connector/hr/hr.py
+++ addons/vnc_zimbra_connector/hr/hr.py
@@ -101,4 +101,24 @@ class hr_employee(osv.osv):
             self.write(cr, uid, data['id'], {'first_name': name_data[0], 'last_name': name_data[-1]},\
                             context)
         return True
+    
+class res_company(osv.osv):
+    _inherit = 'res.company'
+    
+    def name_get(self, cr, uid, ids, context=None):
+        if not len(ids):
+            return []
+        res = []
+        for comps in self.browse(cr, uid, ids, context=context):
+            c_name = comps.code and '[' + comps.code + '] ' or ''
+            c_name += comps.name
+            res.append((comps.id,c_name))
+        return res
+    
+    _columns = {
+                'code': fields.char('Code', size=32)
+    }
+                
+                
+    
         

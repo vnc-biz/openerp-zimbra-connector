5a5628409f91157624e8fd390a64ccaf375fa540
Ekta Dakwala <ekta.dakwala@vnc.biz>
2016-04-22 12:46:43 +0530

    [IMP] vnc_zimbra_connector_extra : added new module to add fields and views, needded to add in standard odoo and not in internal instance

--- addons/vnc_zimbra_connector/hr/hr.py
+++ addons/vnc_zimbra_connector/hr/hr.py
@@ -102,22 +102,6 @@ class hr_employee(osv.osv):
                             context)
         return True
     
-class res_company(osv.osv):
-    _inherit = 'res.company'
-    
-    def name_get(self, cr, uid, ids, context=None):
-        if not len(ids):
-            return []
-        res = []
-        for comps in self.browse(cr, uid, ids, context=context):
-            c_name = comps.code and '[' + comps.code + '] ' or ''
-            c_name += comps.name
-            res.append((comps.id,c_name))
-        return res
-    
-    _columns = {
-                'code': fields.char('Code', size=32)
-    }
                 
                 
     
